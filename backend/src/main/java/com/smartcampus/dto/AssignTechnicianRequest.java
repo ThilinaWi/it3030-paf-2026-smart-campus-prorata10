51b7a6c4a17cf5b8cc180bdb0ff09035e5792c8d
Isira Fernando <isirafernandoe@gmail.com>
2026-04-25 21:31:03 +0530

    AssignTechnicianRequest.java - created file and updated

--- backend/src/main/java/com/smartcampus/dto/AssignTechnicianRequest.java
+++ backend/src/main/java/com/smartcampus/dto/AssignTechnicianRequest.java
@@ -1,19 +1,23 @@
 package com.smartcampus.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank; // Ensures the field is not empty
 
 public class AssignTechnicianRequest {
 
-    @NotBlank(message = "Technician ID is required")
+    //  ID of the technician to be assigned to the ticket
+    @NotBlank(message = "Technician ID is required") // Must provide technician ID
     private String technicianId;
 
+    // Default constructor
     public AssignTechnicianRequest() {
     }
 
+    // Getter for technician ID
     public String getTechnicianId() {
         return technicianId;
     }
 
+    // Setter for technician ID
     public void setTechnicianId(String technicianId) {
         this.technicianId = technicianId;
     }
